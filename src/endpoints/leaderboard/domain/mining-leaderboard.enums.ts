73fdd2a245e7b2baf890c274302a6735d6e93080
rkamysz <radoslaw.kamysz@gmail.com>
2023-03-20 14:47:40 +0100

    removed unused values in sort enum

--- src/endpoints/leaderboard/domain/mining-leaderboard.enums.ts
+++ src/endpoints/leaderboard/domain/mining-leaderboard.enums.ts
@@ -7,14 +7,12 @@ export enum MiningLeaderboardTimeframe {
 
 export enum MiningLeaderboardSort {
   TlmGainsTotal = 'tlm_gains_total',
-  TlmGainsHighest = 'tlm_gains_highest',
   TotalNftPoints = 'total_nft_points',
   AvgChargeTime = 'avg_charge_time',
   AvgMiningPower = 'avg_mining_power',
   AvgNftPower = 'avg_nft_power',
   LandsMinedOn = 'lands_mined_on',
   PlanetsMinedOn = 'planets_mined_on',
-  MineRating = 'mine_rating',
   UniqueToolsUsed = 'unique_tools_used',
 }
 
